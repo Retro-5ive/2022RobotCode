d349a2b40dd4adb4cc9ba85e579c0ce5b8d929a4
Retro-5ive FRC Team 6914 <79761636+Retro-5ive@users.noreply.github.com>
2022-01-29 11:06:26 -0500

    First commit

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,76 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+#include <frc/GenericHID.h>
+#include <frc/TimedRobot.h>
+#include <ctre/phoenix/motorcontrol/can/WPI_TalonSRX.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/SpeedControllerGroup.h>
+#include <frc/Joystick.h> 
+#include "rev/SparkMaxPIDController.h"
+#include <frc/motorcontrol/PWMSparkMax.h>
+
+
+
+class Robot : public frc::TimedRobot {      
+
+
+//Talon drive motors initialization and groupings
+ctre::phoenix::motorcontrol::can::WPI_TalonSRX leftf{1};;   //left front motor is a talon
+ctre::phoenix::motorcontrol::can::WPI_TalonSRX leftb{5};    //left back motor is a talon
+frc::SpeedControllerGroup leftDrive{leftf, leftb};          //combine front and back motors
+
+ctre::phoenix::motorcontrol::can::WPI_TalonSRX rightf{2};   //right front motor is a talon
+ctre::phoenix::motorcontrol::can::WPI_TalonSRX rightb{4};   //right back motor is a talon
+frc::SpeedControllerGroup rightDrive{rightf, rightb};       //combine front and back motors
+
+frc::DifferentialDrive tankDrive{leftDrive, rightDrive};    //make left side and right side into one drive - tank drive
+
+
+//Talon mechanism motors initialization
+ctre::phoenix::motorcontrol::can::WPI_TalonSRX intake{0};
+ctre::phoenix::motorcontrol::can::WPI_TalonSRX outtake{3};
+ctre::phoenix::motorcontrol::can::WPI_TalonSRX shooter{6};
+ctre::phoenix::motorcontrol::can::WPI_TalonSRX climberDown{7};
+
+
+//SparkMax and Reg spark initialization
+frc::PWMSparkMax index2{9};
+frc::PWMSparkMax climberUp{10};
+
+//Joysticks instantiation
+frc::Joystick drivePad{0};      //port 0 is a gamepad for drive
+frc::GenericHID mechPad{1};       //port 1 is a gamepad for mechanisms
+
+public:
+void RobotInit() override {
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    rightDrive.SetInverted(true);
+  }
+
+void TeleopPeriodic() override {
+
+    // Drive with tank style using drivePad
+    tankDrive.TankDrive(drivePad.GetRawAxis(1), drivePad.GetRawAxis(3));  //axis 1 and 3 from drivePad are to gauge the drive
+
+    //attach mechanisms to mechpad
+    climberUp.Set(mechPad.GetPOV(0));
+    climberDown.Set(mechPad.GetPOV(180));
+    index2.Set(mechPad.GetRawButton(1));
+    intake.Set(mechPad.GetRawButton(6));
+    outtake.Set(mechPad.GetRawButton(5));
+    shooter.Set(mechPad.GetRawButton(8));
+
+  }
+};
+
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
+
+
