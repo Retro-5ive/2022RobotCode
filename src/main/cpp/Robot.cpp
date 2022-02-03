4249bfbc50c2dee39cd1966557c8b1ebb273aaa6
Retro-5ive FRC Team 6914 <79761636+Retro-5ive@users.noreply.github.com>
2022-02-03 17:52:58 -0500

    Added ctre and rev libraries

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -16,27 +16,26 @@ class Robot : public frc::TimedRobot {
 
 
 //Talon drive motors initialization and groupings
-ctre::phoenix::motorcontrol::can::WPI_TalonSRX leftf{1};;   //left front motor is a talon
-ctre::phoenix::motorcontrol::can::WPI_TalonSRX leftb{5};    //left back motor is a talon
-frc::SpeedControllerGroup leftDrive{leftf, leftb};          //combine front and back motors
+ctre::phoenix::motorcontrol::can::WPI_TalonSRX leftf{3};  //left back motor is a talon
+frc::SpeedControllerGroup leftDrive{leftf};          //combine front and back motors
 
-ctre::phoenix::motorcontrol::can::WPI_TalonSRX rightf{2};   //right front motor is a talon
-ctre::phoenix::motorcontrol::can::WPI_TalonSRX rightb{4};   //right back motor is a talon
-frc::SpeedControllerGroup rightDrive{rightf, rightb};       //combine front and back motors
+ctre::phoenix::motorcontrol::can::WPI_TalonSRX rightf{6};   //right front motor is a talon
+
+frc::SpeedControllerGroup rightDrive{rightf};       //combine front and back motors
 
 frc::DifferentialDrive tankDrive{leftDrive, rightDrive};    //make left side and right side into one drive - tank drive
 
 
 //Talon mechanism motors initialization
-ctre::phoenix::motorcontrol::can::WPI_TalonSRX intake{0};
-ctre::phoenix::motorcontrol::can::WPI_TalonSRX outtake{3};
-ctre::phoenix::motorcontrol::can::WPI_TalonSRX shooter{6};
-ctre::phoenix::motorcontrol::can::WPI_TalonSRX climberDown{7};
+ctre::phoenix::motorcontrol::can::WPI_TalonSRX intake{1};
+//ctre::phoenix::motorcontrol::can::WPI_TalonSRX outtake{3};
+ctre::phoenix::motorcontrol::can::WPI_TalonSRX shooter{0};
+ctre::phoenix::motorcontrol::can::WPI_TalonSRX climberDown{2};
 
 
 //SparkMax and Reg spark initialization
-frc::PWMSparkMax index2{9};
-frc::PWMSparkMax climberUp{10};
+frc::PWMSparkMax index2{8};
+//frc::PWMSparkMax climberUp{10};
 
 //Joysticks instantiation
 frc::Joystick drivePad{0};      //port 0 is a gamepad for drive
@@ -47,7 +46,8 @@ void RobotInit() override {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    rightDrive.SetInverted(true);
+    leftDrive.SetInverted(true);
+   // shooter.Set(PercentOutput, 1);
   }
 
 void TeleopPeriodic() override {
@@ -56,11 +56,11 @@ void TeleopPeriodic() override {
     tankDrive.TankDrive(drivePad.GetRawAxis(1), drivePad.GetRawAxis(3));  //axis 1 and 3 from drivePad are to gauge the drive
 
     //attach mechanisms to mechpad
-    climberUp.Set(mechPad.GetPOV(0));
-    climberDown.Set(mechPad.GetPOV(180));
+    //climberUp.Set(mechPad.GetPOV(0));
+    //climberDown.Set(mechPad.GetPOV(180));
     index2.Set(mechPad.GetRawButton(1));
     intake.Set(mechPad.GetRawButton(6));
-    outtake.Set(mechPad.GetRawButton(5));
+    //outtake.Set(mechPad.GetRawButton(5));
     shooter.Set(mechPad.GetRawButton(8));
 
   }
